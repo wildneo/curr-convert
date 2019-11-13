81714ab5fed9fc790f78b2bcff81b8250d0e26d1
Roman Oskolkov <wild_neo@mail.ru>
2019-11-13 17:31:34 +0500

    create convert widget component

---
+++ src/components/ConvertWidget.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Divider, Segment, Grid, Container, Icon } from 'semantic-ui-react';
+import CurrencySecond from './CurrencySecond';
+import CurrencyFirst from './CurrencyFirst';
+import * as actions from '../actions';
+
+const mapStateToProps = ({ fetchCurrenciesState }) => ({
+  fetchCurrenciesState
+});
+
+const actionsList = {
+  switchInputs: actions.switchInputs,
+}
+
+class ConvertWidget extends React.Component {
+  handleClick = () => {
+    const { switchInputs } = this.props;
+    switchInputs();
+  }
+
+  render() {
+    const { fetchCurrenciesState } = this.props;
+    const isFetching = fetchCurrenciesState === 'requested';
+
+    return (
+      <Segment
+        loading={isFetching}
+        // inverted
+        // color="teal"
+        // basic
+        // style={{ background: 'white' }}
+      >
+        <Grid columns="equal" stackable>
+          <Grid.Column>
+            <CurrencyFirst />
+          </Grid.Column>
+          <Grid.Column width="one">
+            <Divider vertical>
+              <Icon
+                onClick={this.handleClick}
+                link
+                name="refresh"
+              />
+            </Divider>
+          </Grid.Column>
+          <Grid.Column>
+            <CurrencySecond />
+          </Grid.Column>
+        </Grid>
+      </Segment>
+    );
+  }
+};
+
+
+export default connect(mapStateToProps, actionsList)(ConvertWidget);

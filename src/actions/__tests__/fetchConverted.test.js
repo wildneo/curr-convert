5ed07e9bda0008927c7b2783ebdc2162e612898b
Roman Oskolkov <wild_neo@mail.ru>
2020-01-14 18:08:22 +0500

    add convert action test

---
+++ src/actions/__tests__/fetchConverted.test.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store'
+import * as actions from '../../actions';
+
+jest.mock('axios');
+const mockStore = configureMockStore([
+  thunk,
+]);
+
+describe('fetchConverted', () => {
+  const rate = 688.6;
+  const getRequest = () => {
+    const data = {
+      amount: rate,
+    };
+  
+    return axios.request
+      .mockResolvedValueOnce({ data })
+      .mockRejectedValueOnce();
+  };
+  
+  test('Request success', () => {
+    const store = mockStore();
+    const expectedActions = [
+      actions.fetchConvertedRequest(),
+      actions.fetchConvertedSuccess({ rate }),
+    ];
+    getRequest();
+    return store.dispatch(actions.fetchConverted())
+      .then(() => expect(store.getActions()).toEqual(expectedActions));
+  });
+  
+  test('Request failure', () => {
+    const store = mockStore();
+    const expectedActions = [
+      actions.fetchConvertedRequest(),
+      actions.fetchConvertedFailure(),
+    ];
+    getRequest();
+    return store.dispatch(actions.fetchConverted())
+      .catch(() => expect(store.getActions()).toEqual(expectedActions));
+  });
+  
+});

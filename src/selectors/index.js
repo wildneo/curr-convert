d10edfc0a8d5790e0224820fb4d8a63d1f0baaf6
Roman Oskolkov <wild_neo@mail.ru>
2020-01-20 18:16:54 +0500

    add a filter selector

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,9 +1,20 @@
 import { createSelector } from 'reselect';
+import { searchFilter } from '../utils';
 
 export const getCurrencyByCode = (state) => state.currencies.byCode;
 export const getCurrencyCodes = (state) => state.currencies.allCodes;
+export const getSearchQuery = (state) => state.search.searchQuery;
 
 export const currencySelector = createSelector(
   [getCurrencyByCode, getCurrencyCodes],
   (byCode, allCodes) => allCodes.map((code) => byCode[code]),
+);
+
+export const filteredCurrencySelector = createSelector(
+  [currencySelector, getSearchQuery],
+  (currencies, searchQuery) => (
+    searchQuery
+      ? searchFilter(currencies, searchQuery)
+      : currencies
+  ),
 );

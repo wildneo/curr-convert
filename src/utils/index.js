54bf7be6844eb552ffbf7615b2ec9965f1a0ff31
Roman Oskolkov <wild_neo@mail.ru>
2020-01-20 18:13:46 +0500

    add utility for currency filtration

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,17 @@
-const mask = (value) => {
+import { escapeRegExp } from 'lodash';
+
+export const mask = (value) => {
   return value
     .replace(/,/g, '.')
     .replace(/^\./g, '0.')
     .replace(/[^0-9.]|(?<=\.\d{2})\d*|\.(?=.*\.)/g, '')
 };
 
-export default mask;
+export const searchFilter = (currencies, query) => {
+  const exp = new RegExp(escapeRegExp(query), 'i');
+  const predicate = currency => (
+    exp.test(currency.code) || exp.test(currency.name)
+  );
+
+  return currencies.filter(predicate);
+};
